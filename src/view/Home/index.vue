a4250a824308666186cc2f891e08607f5195b6e3
engravesunny <3170557037@qq.com>
2023-02-06 14:11:44 +0800

    顶部初始化

--- src/view/Home/index.vue
+++ src/view/Home/index.vue
@@ -1,4 +1,3 @@
 <template>
     首页
-    <el-button type="primary">这是一个button按钮</el-button>
 </template>
---
+++ src/view/Layout/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+
+    <div class="top_menu">
+        <!-- logo -->
+        <div class="logo">111</div>
+        <!-- logo -->
+
+        <!-- 搜索框 -->
+        <div class="search"></div>
+        <!-- 搜索框 -->
+
+        <!-- 用户头像 -->
+        <div class="user"></div>
+        <!-- 用户头像 -->
+    </div>
+
+</template>
+
+<style lang="less">
+.top_menu{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 50px;
+    box-sizing: border-box;
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100px;
+    background: #000;
+    .logo{
+        width: 50px;
+        height: 50px;
+        background-color: #fff;
+    }
+    .search{
+        width: 50%;
+        height: 50px;
+        background-color: #fff;
+    }
+    .user{
+        width: 50px;
+        height: 50px;
+        background-color: #fff;
+    }
+}
+</style>
